7c1ffb0ea5f469bb343b454101e17f707f02a0ae
Natanael <natanael.santana@outlook.com.br>
2023-07-01 10:38:05 -0300

    broche model changed, now it require a product description

--- models/broche.js
+++ models/broche.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const brocheSchema = new mongoose.Schema({
   nome: { type: String, required: true },
   imagem: { type: String, required: true },
   preco: { type: Number, required: true },
-  idUnico: { type: String, required: true, unique: true }
+  descricao: { type: String, required: true, unique: true },
 });
 
-module.exports = mongoose.model('Broche', brocheSchema);
+module.exports = mongoose.model("Broche", brocheSchema);
